e78e5e6c96412408e10a8891609c7337d958f463
João Martinho <joao@clovermail.net>
2025-09-04 20:18:34 -0300

    melhorar html

--- src/main/resources/static/js/termoDeCompromisso.js
+++ src/main/resources/static/js/termoDeCompromisso.js
@@ -1,9 +1,12 @@
 const coorientadorCheckbox = document.getElementById('coorientadorCheckbox');
-  const coorientadorMenu = document.getElementById('coorientadorMenu');
-  coorientadorCheckbox.addEventListener('change', () => {
-    if (coorientadorCheckbox.checked) {
-      new bootstrap.Collapse(coorientadorMenu, { show: true });
-    } else {
-      new bootstrap.Collapse(coorientadorMenu, { hide: true });
+    const coorientadorMenu = document.getElementById('coorientadorMenu');
+    coorientadorCheckbox.addEventListener('change', () => {
+
+    if (coorientadorCheckbox.checkVisibility) {
+        new bootstrap.Collapse(coorientadorMenu, { show: true });
     }
-  });
+    else {
+        new bootstrap.Collapse(coorientadorMenu, { hide: true});
+    }
+
+});
